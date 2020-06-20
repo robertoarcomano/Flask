da724f13ff9c0f6afdf3d6fad5fbd2ea65a6148d
Roberto Arcomano <bertolinux@gmail.com>
2020-06-21 00:59:33 +0100

    Added new parameter

--- index.py
+++ index.py
@@ -4,10 +4,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def hello_world(param1=None):
+    return 'Hello, World! Param1: ' + str(param1)
 
-@app.route('/article')
-def article():
-    return 'This is article'
+@app.route('/article/<newone>')
+def article(param1=None,newone=None):
+    return 'This is article. Param1: ' + str(param1)  + " newpath: " + str(newone)
 
